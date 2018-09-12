566b82fb0744a9c3e2c4256c1bfb35514406d837
DerianW <ScratchGobo@gmail.com>
2018-09-12 13:59:01 -0400

    trying again to add a lmao

--- src/main/java/class1.java
+++ src/main/java/class1.java
@@ -2,6 +2,7 @@ public class class1
 {
     public static void main(String[] args)
     {
-        System.out.println("lol");
+        System.out.println("lol") ;
+        System.out.println("lmao") ;
     }
 }
